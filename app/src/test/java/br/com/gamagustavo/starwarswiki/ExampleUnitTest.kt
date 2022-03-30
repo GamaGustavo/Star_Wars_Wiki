82350e70d02b33c76dc18350486bffe59871378d
GamaGustavo <gustavo98ag@gmail.com>
2022-03-30 00:26:41 -0300

    Start. Criação do projeto com uma tela vazia.

---
+++ app/src/test/java/br/com/gamagustavo/starwarswiki/ExampleUnitTest.kt
@@ -0,0 +1,17 @@
+package br.com.gamagustavo.starwarswiki
+
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        val soma = 2 + 2
+        assertEquals(4, soma)
+    }
+}
